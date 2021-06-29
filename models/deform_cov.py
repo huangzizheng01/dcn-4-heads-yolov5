0057782302e0732a698cea09c45eb16cb815e47d
YEARNLL <36359955+YEARNLL@users.noreply.github.com>
2021-06-29 12:59:01 +0800

    Add files via upload

---
+++ models/deform_cov.py
@@ -0,0 +1,227 @@
+from __future__ import absolute_import, division
+
+import torch
+import torch.nn as nn
+
+import numpy as np
+from .layers import th_batch_map_offsets, th_generate_grid
+
+
+class ConvOffset2D(nn.Conv2d):
+    """ConvOffset2D
+    Convolutional layer responsible for learning the 2D offsets and output the
+    deformed feature map using bilinear interpolation
+    Note that this layer does not perform convolution on the deformed feature
+    map. See get_deform_cnn in cnn.py for usage
+    """
+    def __init__(self, filters, init_normal_stddev=0.01, **kwargs):
+        """Init
+        Parameters
+        ----------
+        filters : int
+            Number of channel of the input feature map
+        init_normal_stddev : float
+            Normal kernel initialization
+        **kwargs:
+            Pass to superclass. See Con2d layer in pytorch
+        """
+        self.filters = filters
+        self._grid_param = None
+        super(ConvOffset2D, self).__init__(self.filters, self.filters*2, 3,stride=1, padding=1, bias=False, **kwargs)
+        self.weight.data.copy_(self._init_weights(self.weight, init_normal_stddev))
+
+    def forward(self, x):
+        """Return the deformed featured map"""
+        x_shape = x.size()
+        offsets = super(ConvOffset2D, self).forward(x)
+
+        # offsets: (b*c, h, w, 2)
+        offsets = self._to_bc_h_w_2(offsets, x_shape)
+
+        # x: (b*c, h, w)
+        x = self._to_bc_h_w(x, x_shape)
+
+        # X_offset: (b*c, h, w)
+        x_offset = th_batch_map_offsets(x, offsets, grid=self._get_grid(self,x))
+
+        # x_offset: (b, h, w, c)
+        x_offset = self._to_b_c_h_w(x_offset, x_shape)
+
+        return x_offset
+
+    @staticmethod
+    def _get_grid(self, x):
+        batch_size, input_height, input_width = x.size(0), x.size(1), x.size(2)
+        dtype, cuda = x.data.type(), x.data.is_cuda
+        if self._grid_param == (batch_size, input_height, input_width, dtype, cuda):
+            return self._grid
+        self._grid_param = (batch_size, input_height, input_width, dtype, cuda)
+        self._grid = th_generate_grid(batch_size, input_height, input_width, dtype, cuda)
+        return self._grid
+
+    @staticmethod
+    def _init_weights(weights, std):
+        fan_out = weights.size(0)
+        fan_in = weights.size(1) * weights.size(2) * weights.size(3)
+        w = np.random.normal(0.0, std, (fan_out, fan_in))
+        return torch.from_numpy(w.reshape(weights.size()))
+
+    @staticmethod
+    def _to_bc_h_w_2(x, x_shape):
+        """(b, 2c, h, w) -> (b*c, h, w, 2)"""
+        x = x.contiguous().view(-1, int(x_shape[2]), int(x_shape[3]), 2)
+        return x
+
+    @staticmethod
+    def _to_bc_h_w(x, x_shape):
+        """(b, c, h, w) -> (b*c, h, w)"""
+        x = x.contiguous().view(-1, int(x_shape[2]), int(x_shape[3]))
+        return x
+
+    @staticmethod
+    def _to_b_c_h_w(x, x_shape):
+        """(b*c, h, w) -> (b, c, h, w)"""
+        x = x.contiguous().view(-1, int(x_shape[1]), int(x_shape[2]), int(x_shape[3]))
+        return x
+
+
+#https://github.com/4uiiurz1/pytorch-deform-conv-v2/blob/master/deform_conv_v2.py
+class DeformConv2d(nn.Module):
+    def __init__(self, inc, outc, kernel_size=3, padding=1, stride=1, bias=None, modulation=False):
+        """
+        Args:
+            modulation (bool, optional): If True, Modulated Defomable Convolution (Deformable ConvNets v2).
+        """
+        super(DeformConv2d, self).__init__()
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.stride = stride
+        self.zero_padding = nn.ZeroPad2d(padding)
+        self.conv = nn.Conv2d(inc, outc, kernel_size=kernel_size, stride=kernel_size, bias=bias)
+
+        self.p_conv = nn.Conv2d(inc, 2*kernel_size*kernel_size, kernel_size=3, padding=1, stride=stride)
+        nn.init.constant_(self.p_conv.weight, 0)
+        self.p_conv.register_backward_hook(self._set_lr)
+
+        self.modulation = modulation
+        if modulation:
+            self.m_conv = nn.Conv2d(inc, kernel_size*kernel_size, kernel_size=3, padding=1, stride=stride)
+            nn.init.constant_(self.m_conv.weight, 0)
+            self.m_conv.register_backward_hook(self._set_lr)
+
+    @staticmethod
+    def _set_lr(module, grad_input, grad_output):
+        grad_input = (grad_input[i] * 0.1 for i in range(len(grad_input)))
+        grad_output = (grad_output[i] * 0.1 for i in range(len(grad_output)))
+
+    def forward(self, x):
+        offset = self.p_conv(x)
+        if self.modulation:
+            m = torch.sigmoid(self.m_conv(x))
+
+        dtype = offset.data.type()
+        ks = self.kernel_size
+        N = offset.size(1) // 2
+
+        if self.padding:
+            x = self.zero_padding(x)
+
+        # (b, 2N, h, w)
+        p = self._get_p(offset, dtype)
+
+        # (b, h, w, 2N)
+        p = p.contiguous().permute(0, 2, 3, 1)
+        q_lt = p.detach().floor()
+        q_rb = q_lt + 1
+
+        q_lt = torch.cat([torch.clamp(q_lt[..., :N], 0, x.size(2)-1), torch.clamp(q_lt[..., N:], 0, x.size(3)-1)], dim=-1).long()
+        q_rb = torch.cat([torch.clamp(q_rb[..., :N], 0, x.size(2)-1), torch.clamp(q_rb[..., N:], 0, x.size(3)-1)], dim=-1).long()
+        q_lb = torch.cat([q_lt[..., :N], q_rb[..., N:]], dim=-1)
+        q_rt = torch.cat([q_rb[..., :N], q_lt[..., N:]], dim=-1)
+
+        # clip p
+        p = torch.cat([torch.clamp(p[..., :N], 0, x.size(2)-1), torch.clamp(p[..., N:], 0, x.size(3)-1)], dim=-1)
+
+        # bilinear kernel (b, h, w, N)
+        g_lt = (1 + (q_lt[..., :N].type_as(p) - p[..., :N])) * (1 + (q_lt[..., N:].type_as(p) - p[..., N:]))
+        g_rb = (1 - (q_rb[..., :N].type_as(p) - p[..., :N])) * (1 - (q_rb[..., N:].type_as(p) - p[..., N:]))
+        g_lb = (1 + (q_lb[..., :N].type_as(p) - p[..., :N])) * (1 - (q_lb[..., N:].type_as(p) - p[..., N:]))
+        g_rt = (1 - (q_rt[..., :N].type_as(p) - p[..., :N])) * (1 + (q_rt[..., N:].type_as(p) - p[..., N:]))
+
+        # (b, c, h, w, N)
+        x_q_lt = self._get_x_q(x, q_lt, N)
+        x_q_rb = self._get_x_q(x, q_rb, N)
+        x_q_lb = self._get_x_q(x, q_lb, N)
+        x_q_rt = self._get_x_q(x, q_rt, N)
+
+        # (b, c, h, w, N)
+        x_offset = g_lt.unsqueeze(dim=1) * x_q_lt + \
+                   g_rb.unsqueeze(dim=1) * x_q_rb + \
+                   g_lb.unsqueeze(dim=1) * x_q_lb + \
+                   g_rt.unsqueeze(dim=1) * x_q_rt
+
+        # modulation
+        if self.modulation:
+            m = m.contiguous().permute(0, 2, 3, 1)
+            m = m.unsqueeze(dim=1)
+            m = torch.cat([m for _ in range(x_offset.size(1))], dim=1)
+            x_offset *= m
+
+        x_offset = self._reshape_x_offset(x_offset, ks)
+        out = self.conv(x_offset)
+
+        return out
+
+    def _get_p_n(self, N, dtype):
+        p_n_x, p_n_y = torch.meshgrid(
+            torch.arange(-(self.kernel_size-1)//2, (self.kernel_size-1)//2+1),
+            torch.arange(-(self.kernel_size-1)//2, (self.kernel_size-1)//2+1))
+        # (2N, 1)
+        p_n = torch.cat([torch.flatten(p_n_x), torch.flatten(p_n_y)], 0)
+        p_n = p_n.view(1, 2*N, 1, 1).type(dtype)
+
+        return p_n
+
+    def _get_p_0(self, h, w, N, dtype):
+        p_0_x, p_0_y = torch.meshgrid(
+            torch.arange(1, h*self.stride+1, self.stride),
+            torch.arange(1, w*self.stride+1, self.stride))
+        p_0_x = torch.flatten(p_0_x).view(1, 1, h, w).repeat(1, N, 1, 1)
+        p_0_y = torch.flatten(p_0_y).view(1, 1, h, w).repeat(1, N, 1, 1)
+        p_0 = torch.cat([p_0_x, p_0_y], 1).type(dtype)
+
+        return p_0
+
+    def _get_p(self, offset, dtype):
+        N, h, w = offset.size(1)//2, offset.size(2), offset.size(3)
+
+        # (1, 2N, 1, 1)
+        p_n = self._get_p_n(N, dtype)
+        # (1, 2N, h, w)
+        p_0 = self._get_p_0(h, w, N, dtype)
+        p = p_0 + p_n + offset
+        return p
+
+    def _get_x_q(self, x, q, N):
+        b, h, w, _ = q.size()
+        padded_w = x.size(3)
+        c = x.size(1)
+        # (b, c, h*w)
+        x = x.contiguous().view(b, c, -1)
+
+        # (b, h, w, N)
+        index = q[..., :N]*padded_w + q[..., N:]  # offset_x*w + offset_y
+        # (b, c, h*w*N)
+        index = index.contiguous().unsqueeze(dim=1).expand(-1, c, -1, -1, -1).contiguous().view(b, c, -1)
+
+        x_offset = x.gather(dim=-1, index=index).contiguous().view(b, c, h, w, N)
+
+        return x_offset
+
+    @staticmethod
+    def _reshape_x_offset(x_offset, ks):
+        b, c, h, w, N = x_offset.size()
+        x_offset = torch.cat([x_offset[..., s:s+ks].contiguous().view(b, c, h, w*ks) for s in range(0, N, ks)], dim=-1)
+        x_offset = x_offset.contiguous().view(b, c, h*ks, w*ks)
+
+        return x_offset
---
+++ models/layers.py
@@ -0,0 +1,114 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
+def th_flatten(a):
+    """Flatten tensor"""
+    return a.contiguous().view(a.nelement())
+
+def np_repeat_2d(a, repeats):
+    """Tensorflow version of np.repeat for 2D"""
+
+    assert len(a.shape) == 2
+    a = np.expand_dims(a, 0)
+    a = np.tile(a, [repeats, 1, 1])
+    return a
+
+def th_repeat(a, repeats, axis=0):
+    """Torch version of np.repeat for 1D"""
+    assert len(a.size()) == 1
+    return th_flatten(torch.transpose(a.repeat(repeats, 1), 0, 1))
+
+def th_gather_2d(input, coords):
+    inds = coords[:, 0]*input.size(1) + coords[:, 1]
+    x = torch.index_select(th_flatten(input), 0, inds)
+    return x.view(coords.size(0))
+#双线性插值
+def th_batch_map_coordinates(input, coords, order=1):
+    """Batch version of th_map_coordinates
+    Only supports 2D feature maps
+    Parameters
+    ----------
+    input : tf.Tensor. shape = (b, s, s)
+    coords : tf.Tensor. shape = (b, n_points, 2)
+    Returns
+    -------
+    tf.Tensor. shape = (b, s, s)
+    """
+
+    batch_size = input.size(0)
+    input_height = input.size(1)
+    input_width = input.size(2)
+
+    n_coords = coords.size(1)
+
+    # coords = torch.clamp(coords, 0, input_size - 1)
+
+    coords = torch.cat((torch.clamp(coords.narrow(2, 0, 1), 0, input_height - 1), torch.clamp(coords.narrow(2, 1, 1), 0, input_width - 1)), 2)
+
+    assert (coords.size(1) == n_coords)
+
+    coords_lt = coords.floor().long()
+    coords_rb = coords.ceil().long()
+    coords_lb = torch.stack([coords_lt[..., 0], coords_rb[..., 1]], 2)
+    coords_rt = torch.stack([coords_rb[..., 0], coords_lt[..., 1]], 2)
+    idx = th_repeat(torch.arange(0, batch_size), n_coords).long()
+    idx = Variable(idx, requires_grad=False)
+    if input.is_cuda:
+        idx = idx.cuda()
+
+    def _get_vals_by_coords(input, coords):
+        indices = torch.stack([
+            idx, th_flatten(coords[..., 0]), th_flatten(coords[..., 1])
+        ], 1)
+        inds = indices[:, 0]*input.size(1)*input.size(2)+ indices[:, 1]*input.size(2) + indices[:, 2]
+        vals = th_flatten(input).index_select(0, inds)
+        vals = vals.view(batch_size, n_coords)
+        return vals
+
+    vals_lt = _get_vals_by_coords(input, coords_lt.detach())
+    vals_rb = _get_vals_by_coords(input, coords_rb.detach())
+    vals_lb = _get_vals_by_coords(input, coords_lb.detach())
+    vals_rt = _get_vals_by_coords(input, coords_rt.detach())
+
+    coords_offset_lt = coords - coords_lt.type(coords.data.type())
+    vals_t = coords_offset_lt[..., 0]*(vals_rt - vals_lt) + vals_lt
+    vals_b = coords_offset_lt[..., 0]*(vals_rb - vals_lb) + vals_lb
+    mapped_vals = coords_offset_lt[..., 1]* (vals_b - vals_t) + vals_t
+    return mapped_vals
+
+def th_generate_grid(batch_size, input_height, input_width, dtype, cuda):
+    grid = np.meshgrid(
+        range(input_height), range(input_width), indexing='ij'
+    )
+    grid = np.stack(grid, axis=-1)
+    grid = grid.reshape(-1, 2)
+
+    grid = np_repeat_2d(grid, batch_size)
+    grid = torch.from_numpy(grid).type(dtype)
+    if cuda:
+        grid = grid.cuda()
+    return Variable(grid, requires_grad=False)
+
+
+def th_batch_map_offsets(input, offsets, grid=None, order=1):
+    """Batch map offsets into input
+    Parameters
+    ---------
+    input : torch.Tensor. shape = (b, s, s)
+    offsets: torch.Tensor. shape = (b, s, s, 2)
+    Returns
+    -------
+    torch.Tensor. shape = (b, s, s)
+    """
+    batch_size = input.size(0)
+    input_height = input.size(1)
+    input_width = input.size(2)
+
+    offsets = offsets.view(batch_size, -1, 2)
+    if grid is None:
+        grid = th_generate_grid(batch_size, input_height, input_width, offsets.data.type(), offsets.data.is_cuda)
+
+    coords = offsets + grid
+
+    mapped_vals = th_batch_map_coordinates(input, coords)
+    return mapped_vals
